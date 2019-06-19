783736fc598713a0578dda8ac9c040aef764188e
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2019-06-19 15:33:45 +0200

    Create Simple Responsive GUI

--- index.php
+++ index.php
@@ -8,6 +8,25 @@
     <title>SHOUIT! Shoutbox</title>
 </head>
 <body>
-    
+    <div id="container">
+        <header>
+            <h1>SHOUIT! Shoutbox</h1>
+        </header>
+        <div id="shouts">
+            <ul>
+            <li class="shout"><span>10:15pm</span> - <strong>Ebrahim</strong> : I love Asmaa </li>
+            <li class="shout"><span>10:15pm</span> - <strong>Ebrahim</strong> : I love Asmaa </li>
+            <li class="shout"><span>10:15pm</span> - <strong>Ebrahim</strong> : I love Asmaa </li>
+            <li class="shout"><span>10:15pm</span> - <strong>Ebrahim</strong> : I love Asmaa </li>
+            </ul>
+        </div>
+        <div id="input">
+            <form action="process.php" method="post">
+                <input type="text" name="user" placeholder="Enter Your Name">
+                <input type="text" name="msg" placeholder="Enter Your Message">
+            </form>
+            <input class="shout-btn" type="submit" name="submit" value="Shout it Out !">
+        </div>
+    </div>
 </body>
 </html>
